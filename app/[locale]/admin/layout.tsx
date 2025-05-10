f892cb85132dcb87df16cd8e5ff56dcdd8d4a654
اسمك على GitHub <@.com>
2025-05-10 07:02:41 +0300

    last-2

--- app/[locale]/admin/layout.tsx
+++ app/[locale]/admin/layout.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import AdminSidebar from '@/components/shared/admin/AdminSidebar'
-import AdminHeader from '@/components/shared/admin/AdminHeader' // 👈 الهيدر الجديد
 
 export default function AdminLayout({
   children,
@@ -11,15 +10,7 @@ export default function AdminLayout({
     <div className='flex min-h-screen'>
       {/* Sidebar */}
       <AdminSidebar />
-
-      {/* Content */}
-      <div className='flex flex-col flex-1'>
-        {/* Header */}
-        <AdminHeader /> {/* 👈 الهيدر الجديد */}
-
-        {/* Main Page Content */}
-        <main className='flex-1 p-4'>{children}</main>
-      </div>
+      <main className='flex-1 p-4'>{children}</main>
     </div>
   )
 }
