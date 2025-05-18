e845b0d31d42d331c7599a379667e1d7866e94ea
Mohamedabdulaziz1920 <140448813+Mohamedabdulaziz1920@users.noreply.github.com>
2025-05-18 18:21:20 +0300

    Update page.tsx

--- app/[locale]/admin/categories/create/page.tsx
+++ app/[locale]/admin/categories/create/page.tsx
@@ -3,7 +3,7 @@ import CategoryForm from '../category-form'
 
 export default function CreateCategoryPage() {
   return (
-    <main className='container mx-auto px-4 py-6'>
+       <main className="pt-16 px-4 md:px-6">
       <h1 className='text-xl font-bold mb-6'>إنشاء قسم جديد</h1>
      <CategoryForm type='Create' />
     </main>
