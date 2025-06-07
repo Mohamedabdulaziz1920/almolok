5f1680bfbb4722d5685cd53efd9e6eb75d6d2289
Mohamedabdulaziz1920 <140448813+Mohamedabdulaziz1920@users.noreply.github.com>
2025-06-07 22:05:58 +0300

    Update menu.tsx

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -8,7 +8,7 @@ import {
   SheetTrigger,
 } from '@/components/ui/sheet'
 import CartButton from './cart-button'
-import UserButton from '../admin/UserButton'
+import UserButton from './user-button'
 import ThemeSwitcher from './theme-switcher'
 import LanguageSwitcher from './language-switcher'
 import { useTranslations } from 'next-intl'
