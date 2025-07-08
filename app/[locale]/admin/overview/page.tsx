8069a3062b0e21d9d5ec031191f09eafa196712d
Mohamedabdulaziz1920 <140448813+Mohamedabdulaziz1920@users.noreply.github.com>
2025-07-08 17:12:55 +0300

    Update page.tsx

--- app/[locale]/admin/overview/page.tsx
+++ app/[locale]/admin/overview/page.tsx
@@ -1,27 +1,19 @@
 import { Metadata } from 'next'
-
+import { auth } from '@/auth'
 import OverviewReport from './overview-report'
 
-import { auth } from '@/auth'
-import { useEffect } from 'react';
 export const metadata: Metadata = {
   title: 'Admin Dashboard',
 }
+
 const DashboardPage = async () => {
   const session = await auth()
-  if (session?.user.role !== 'Admin')
+
+  if (session?.user.role !== 'Admin') {
     throw new Error('Admin permission required')
+  }
 
   return <OverviewReport />
 }
-const DashboardPage: React.FC = () => {
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const alreadyRefreshed = sessionStorage.getItem('dashboardRefreshed');
-      if (!alreadyRefreshed) {
-        sessionStorage.setItem('dashboardRefreshed', 'true');
-        window.location.reload();
-      }
-    }
-  }, []);
+
 export default DashboardPage
