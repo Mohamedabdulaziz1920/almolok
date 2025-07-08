7fd5b280079213a0949c18433070b630d70c67dc
Mohamedabdulaziz1920 <140448813+Mohamedabdulaziz1920@users.noreply.github.com>
2025-07-08 17:01:04 +0300

    Update page.tsx

--- app/[locale]/admin/overview/page.tsx
+++ app/[locale]/admin/overview/page.tsx
@@ -1,7 +1,9 @@
 import { Metadata } from 'next'
 
 import OverviewReport from './overview-report'
+
 import { auth } from '@/auth'
+import { useEffect } from 'react';
 export const metadata: Metadata = {
   title: 'Admin Dashboard',
 }
@@ -12,5 +14,14 @@ const DashboardPage = async () => {
 
   return <OverviewReport />
 }
-
+const DashboardPage: React.FC = () => {
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const alreadyRefreshed = sessionStorage.getItem('dashboardRefreshed');
+      if (!alreadyRefreshed) {
+        sessionStorage.setItem('dashboardRefreshed', 'true');
+        window.location.reload();
+      }
+    }
+  }, []);
 export default DashboardPage
