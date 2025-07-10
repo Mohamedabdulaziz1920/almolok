994dc845c2ef000fecd14b0cd9d95ebe9660968f
Mohamedabdulaziz1920 <140448813+Mohamedabdulaziz1920@users.noreply.github.com>
2025-07-10 20:23:20 +0300

    Update layout.tsx

--- app/[locale]/(auth)/layout.tsx
+++ app/[locale]/(auth)/layout.tsx
@@ -9,32 +9,51 @@ export default async function AuthLayout({
   children: React.ReactNode
 }) {
   const { site } = await getSetting()
+
   return (
-    <div className='flex flex-col items-center min-h-screen highlight-link  '>
-      <header className='mt-8'>
-        <Link href='/'>
+  <div className="flex flex-col items-center min-h-screen highlight-link">
+      {/* Header */}
+     <header className="mt-4">
+        <Link href="/">
           <Image
-            src='/icons/logo.svg'
-            alt='logo'
-            width={64}
-            height={64}
+            src="/icons/logo.svg"
+            alt="Logo"
+            width={72}
+            height={72}
             priority
-            style={{
-              maxWidth: '100%',
-              height: 'auto',
-            }}
+            className="rounded-xl drop-shadow-md hover:scale-105 transition-transform"
           />
         </Link>
       </header>
-      <main className='mx-auto max-w-sm min-w-80 p-4'>{children}</main>
-      <footer className=' flex-1 mt-8  bg-gray-800 w-full flex flex-col gap-4 items-center p-8 text-sm'>
-        <div className='flex justify-center space-x-4'>
-          <Link href='/page/conditions-of-use'>Conditions of Use</Link>
-          <Link href='/page/privacy-policy'> Privacy Notice</Link>
-          <Link href='/page/help'> Help </Link>
+
+      {/* Main Content */}
+     <main className="mx-auto max-w-sm min-w-80 p-4">{children}</main>
+ 
+
+      {/* Footer */}
+       <footer className="flex-1 mt-4 bg-gray-800 w-full flex flex-col gap-4 items-center p-6 text-sm text-gray-400">
+        <div className="flex justify-center flex-wrap gap-4 mb-2">
+          <Link
+            href="/page/conditions-of-use"
+            className="hover:text-yellow-500 transition-colors"
+          >
+            Conditions of Use
+          </Link>
+          <Link
+            href="/page/privacy-policy"
+            className="hover:text-yellow-500 transition-colors"
+          >
+            Privacy Notice
+          </Link>
+          <Link
+            href="/page/help"
+            className="hover:text-yellow-500 transition-colors"
+          >
+            Help
+          </Link>
         </div>
         <div>
-          <p className='text-gray-400'>{site.copyright}</p>
+          <p className="text-xs text-gray-500">{site?.copyright}</p>
         </div>
       </footer>
     </div>
