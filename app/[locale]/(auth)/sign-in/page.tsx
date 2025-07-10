9ace9695b31ad9a8fa43d444e63be8f2d8af5025
Mohamedabdulaziz1920 <140448813+Mohamedabdulaziz1920@users.noreply.github.com>
2025-07-10 18:16:35 +0300

    Update page.tsx

--- app/[locale]/(auth)/sign-in/page.tsx
+++ app/[locale]/(auth)/sign-in/page.tsx
@@ -35,12 +35,12 @@ export default async function SignInPage(props: {
 
   return (
     <div className='w-full min-h-screen flex items-center justify-center bg-gradient-to-br from-gray-50 to-gray-100 px-4 py-12'>
-      <div className='max-w-md w-full space-y-6'>
+      <div className='max-w-md w-full space-y-2'>
         <Card className='shadow-xl rounded-2xl border border-gray-200'>
           <CardHeader className='text-center'>
-            <CardTitle className='text-3xl font-semibold text-gray-800 flex items-center justify-center gap-2'>
+            <CardTitle className='text-3xl font-semibold text-yellow flex items-center justify-center gap-2'>
               {t('title')}
-              <LogIn className='w-6 h-6 text-blue-500 transform rotate-180' />
+              <LogIn className='w-6 h-6 text-yellow transform rotate-180' />
             </CardTitle>
           </CardHeader>
           <CardContent className='space-y-4'>
